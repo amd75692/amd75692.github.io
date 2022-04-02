f6d8b58aaa4dd3eaf343afc9a45535b470f2edfb
ahmedemaouloud <ahmed.maouloud@gmail.com>
2022-04-02 21:02:00 +0200

    :fire: fix: css

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: './',
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
