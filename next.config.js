474d0a57e461169d8201e70e7e3bc75ae8530d98
ahmedemaouloud <ahmed.maouloud@gmail.com>
2022-04-02 18:44:52 +0200

    :fire: feat: setting path & prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 module.exports = {
+  prefix,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   sassOptions: {
